e2e8d1739ea15f364f63e00c509438fd9037d779
Teodor <teedo966@gmail.com>
2024-11-10 08:55:27 +0200

    Dictionaries-exc

---
+++ Dictionaries/07.word_synonims.py
@@ -0,0 +1,11 @@
+count_synonym = int(input())
+synonyms = {}
+
+for _ in range(count_synonym):
+    word = input()
+    synonym = input()
+    if word not in synonyms:
+        synonyms[word] = []
+    synonyms[word].append(synonym)
+for word in synonyms:
+    print(f"{word} - {', '.join(synonyms[word])}")
