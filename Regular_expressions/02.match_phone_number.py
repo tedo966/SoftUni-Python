d6cebe461a18047fa28227dd8124fe6ca2d72206
Teodor <teedo966@gmail.com>
2024-11-20 07:50:03 +0200

    Regular-expression-exc

---
+++ Regular_expressions/02.match_phone_number.py
@@ -0,0 +1,11 @@
+import re
+phone_numbers = input()
+pattern = r'\+359-2-\d{3}-\d{4}\b|\+359 2 \d{3} \d{4}\b'
+
+valid_numbers = re.findall(pattern, phone_numbers)
+
+for i in range(len(valid_numbers)):
+    if i < len(valid_numbers) - 1:
+        print(valid_numbers[i], end=", ")
+    else:
+        print(valid_numbers[i])
